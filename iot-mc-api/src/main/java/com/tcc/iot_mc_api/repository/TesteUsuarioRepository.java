be89007cc28cda6445b3a8d0ae8769d45e99a6f0
nicolas <nicolaslorenzi2008@gmail.com>
2025-04-22 17:23:15 -0300

    Teste de usuario

---
+++ iot-mc-api/src/main/java/com/tcc/iot_mc_api/repository/TesteUsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.tcc.iot_mc_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tcc.iot_mc_api.model.TesteUsuario;
+
+@Repository
+public interface TesteUsuarioRepository extends JpaRepository<TesteUsuario, Long>{
+    TesteUsuario findByUsername(String nome);
+    boolean existsByUsername(String nome);
+}
